37be84dfffcc45466db05411c73503307543497d
Alex Kovetskiy <Alexey007gold@mail.ru>
2017-03-24 10:41:20 +0200

    Split MyRestController to PublicRestController and PrivateRestController, moved InvalidTokenException to common-exceptions module.

--- frontend/rest-frontend/src/main/java/com/alexkoveckiy/frontend/rest/security/Filter.java
+++ frontend/rest-frontend/src/main/java/com/alexkoveckiy/frontend/rest/security/Filter.java
@@ -2,17 +2,21 @@ package com.alexkoveckiy.frontend.rest.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
-import com.alexkoveckiy.common.token.api.TokenHandler;
+//import com.alexkoveckiy.common.token.api.TokenHandler;
+//import com.alexkoveckiy.common.exceptions.InvalidTokenException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -24,20 +28,23 @@ public class Filter extends UsernamePasswordAuthenticationFilter {
     @Autowired
     private TokenHandler tokenHandler;
 
-    private TokenAuthentication tokenAuthentication;
+    private Authentication authentication;
 
     public Filter() {
-        this.tokenAuthentication = new TokenAuthentication();
+        this.authentication = new UsernamePasswordAuthenticationToken(null, null);
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         try {
-            ((HttpServletRequest)req).getSession()
-                    .setAttribute("routing_data", tokenHandler.getRoutingDataFromTemporaryToken(req.getParameter("x-token")));
-            SecurityContextHolder.getContext().setAuthentication(tokenAuthentication);
+            HttpServletRequest request = ((HttpServletRequest)req);
+            request.getSession()
+                    .setAttribute("routing_data", tokenHandler.getRoutingDataFromTemporaryToken(request.getHeader("x-token")));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             chain.doFilter(req, res);
-        } catch (InvalidTokenException ignored) {}
+        } catch (InvalidTokenException ignored) {
+            ((HttpServletResponse) res).sendError(403);
+        }
     }
 
     @Override
