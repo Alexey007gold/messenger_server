67a0d18b58fe4b4d797031e183fc4a1dfb3f814a
Alex Kovetskiy <Alexey007gold@mail.ru>
2017-03-27 12:12:12 +0300

    Corrections

--- basis/common-dao/src/main/java/com/alexkoveckiy/common/dao/entities/BaseEntity.java
+++ basis/common-dao/src/main/java/com/alexkoveckiy/common/dao/entities/BaseEntity.java
@@ -1,11 +1,13 @@
 package com.alexkoveckiy.common.dao.entities;
 
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by alex on 27.03.17.
  */
+@MappedSuperclass
 public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = -4097791966361292473L;
     
