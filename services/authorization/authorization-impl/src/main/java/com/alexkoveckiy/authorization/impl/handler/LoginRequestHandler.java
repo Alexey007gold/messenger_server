f589ab6b2d07da3690289e69da72e84da71a4ce4
Alex Kovetskiy <Alexey007gold@mail.ru>
2017-04-07 15:25:34 +0300

    Changes according to ResponseFactory

--- services/authorization/authorization-impl/src/main/java/com/alexkoveckiy/authorization/impl/handler/LoginRequestHandler.java
+++ services/authorization/authorization-impl/src/main/java/com/alexkoveckiy/authorization/impl/handler/LoginRequestHandler.java
@@ -4,19 +4,17 @@ import com.alexkoveckiy.authorization.api.message.LoginRequest;
 import com.alexkoveckiy.authorization.api.message.LoginResponse;
 import com.alexkoveckiy.authorization.api.router.AuthorizationRequestHandler;
 import com.alexkoveckiy.common.dao.service.DeviceService;
-import com.alexkoveckiy.common.exceptions.InvalidTokenException;
-import com.alexkoveckiy.common.protocol.ActionHeader;
 import com.alexkoveckiy.common.protocol.Request;
 import com.alexkoveckiy.common.protocol.Response;
-import com.alexkoveckiy.common.protocol.ResponseStatus;
+import com.alexkoveckiy.common.protocol.ResponseFactory;
 import com.alexkoveckiy.common.router.api.AbstractRequestHandler;
 import com.alexkoveckiy.common.token.api.TokenHandler;
 import org.jose4j.jwt.JwtClaims;
-import org.jose4j.jwt.MalformedClaimException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
+import static com.alexkoveckiy.common.token.impl.TokenHandlerImpl.Claim.DEVICE_ID;
+import static com.alexkoveckiy.common.token.impl.TokenHandlerImpl.Claim.PROFILE_ID;
 
 /**
  * Created by alex on 24.02.17.
@@ -37,32 +35,12 @@ public class LoginRequestHandler extends AbstractRequestHandler<LoginRequest, Lo
     }
 
     @Override
-    public Response<LoginResponse> process(Request<LoginRequest> msg) {
-        ActionHeader header;
-        LoginResponse data;
-        ResponseStatus status;
-
-        try {
-            String deviceToken = msg.getData().getDeviceToken();
-            JwtClaims claims = tokenHandler.getClaimsFromDeviceToken(deviceToken);
-            String userId = claims.getStringClaimValue("user_id");
-            String deviceId = claims.getStringClaimValue("device_id");
-            if (claims.getIssuedAt().getValueInMillis()
-                    == deviceService.findByProfileIdAndDeviceId(userId, deviceId).getConfirmationTime()) {
-                header = new ActionHeader(UUID.randomUUID().toString(),
-                        msg.getHeader().getUuid(),
-                        "login",
-                        "authorization",
-                        "HTTP/1.1");
-                data = new LoginResponse(tokenHandler.createTemporaryToken(userId, deviceId));
-                status = new ResponseStatus(200, "OK");
-            } else
-                throw new InvalidTokenException();
-        } catch (InvalidTokenException | MalformedClaimException e) {
-            header = null;
-            data = null;
-            status = new ResponseStatus(403, "Forbidden");
-        }
-        return new Response<>(header, data, status);
+    public Response<LoginResponse> process(Request<LoginRequest> msg) throws Exception {
+        JwtClaims claims = tokenHandler.getClaimsFromDeviceToken(msg.getData().getDeviceToken());
+        String profileId = claims.getStringClaimValue(PROFILE_ID);
+        String deviceId = claims.getStringClaimValue(DEVICE_ID);
+        if (claims.getIssuedAt().getValueInMillis() == deviceService.findByProfileIdAndDeviceId(profileId, deviceId).getConfirmationTime())
+            return ResponseFactory.createResponse(msg, new LoginResponse(tokenHandler.createTemporaryToken(profileId, deviceId)));
+        return ResponseFactory.createResponse(msg, ResponseFactory.Status.FORBIDDEN);
     }
 }
--- services/authorization/authorization-impl/src/main/java/com/alexkoveckiy/authorization/impl/handler/SmsConfirmRequestHandler.java
+++ services/authorization/authorization-impl/src/main/java/com/alexkoveckiy/authorization/impl/handler/SmsConfirmRequestHandler.java
@@ -13,16 +13,15 @@ import com.alexkoveckiy.common.dao.service.DeviceService;
 import com.alexkoveckiy.common.dao.service.ProfileService;
 import com.alexkoveckiy.common.dao.service.ProfileSettingsService;
 import com.alexkoveckiy.common.dao.service.ProfileStatusService;
-import com.alexkoveckiy.common.protocol.ActionHeader;
 import com.alexkoveckiy.common.protocol.Request;
 import com.alexkoveckiy.common.protocol.Response;
-import com.alexkoveckiy.common.protocol.ResponseStatus;
+import com.alexkoveckiy.common.protocol.ResponseFactory;
 import com.alexkoveckiy.common.router.api.AbstractRequestHandler;
 import com.alexkoveckiy.common.token.api.TokenHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
+import static com.alexkoveckiy.common.protocol.ResponseFactory.Status.FORBIDDEN;
 
 /**
  * Created by alex on 24.02.17.
@@ -49,56 +48,32 @@ public class SmsConfirmRequestHandler extends AbstractRequestHandler<SmsConfirmR
     @Autowired
     private ProfileStatusService profileStatusService;
 
-    @Autowired
-
     @Override
     public String getName() {
         return "sms_confirm";
     }
 
     @Override
-    public Response<SmsConfirmResponse> process(Request<SmsConfirmRequest> msg) {
-        ActionHeader header = null;
-        SmsConfirmResponse data = null;
-        ResponseStatus status = null;
+    public Response<SmsConfirmResponse> process(Request<SmsConfirmRequest> msg) throws Exception {
+        RegSession regSession = regSessions.takeRegSession(msg.getData().getRegistrationRequestUuid());
+        if (regSession == null || regSession.getAuthCode() != msg.getData().getAuthCode())
+            return ResponseFactory.createResponse(msg, FORBIDDEN);
 
-        try {
-            String uuid = msg.getData().getRegistrationRequestUuid();
-            int authCode = msg.getData().getAuthCode();
-
-            RegSession regSession = regSessions.takeRegSession(uuid);
-            if (regSession != null && regSession.getAuthCode() == authCode) {
-                long time = System.currentTimeMillis();
-                ProfileEntity profileEntity = profileService.save(new ProfileEntity(regSession.getPhoneNumber(),
-                        time,
-                        time,
-                        null,
-                        null,
-                        null));
-                String deviceToken = tokenHandler.createDeviceToken(profileEntity.getId(),
-                        regSession.getDeviceId(),
-                        regSession.getLocale());
-
-                deviceService.save(new DeviceEntity(profileEntity.getId(),
-                        regSession.getDeviceId(),
-                        tokenHandler.getClaimsFromDeviceToken(deviceToken).getIssuedAt().getValueInMillis()));
-                profileSettingsService.save(new ProfileSettingsEntity(profileEntity.getId(), true, true));
-                profileStatusService.save(new ProfileStatusEntity(profileEntity.getId(), null));
-
-                header = new ActionHeader(UUID.randomUUID().toString(),
-                        msg.getHeader().getUuid(),
-                        "sms_confirm",
-                        "authorization",
-                        "HTTP/1.1");
-                data = new SmsConfirmResponse(deviceToken);
-                status = new ResponseStatus(200, "OK");
-            } else {
-                status = new ResponseStatus(403, "Forbidden");
-            }
-        } catch (Exception e) {
-            status = new ResponseStatus(400, "Bad request");
+        ProfileEntity profileEntity = profileService.findByPhoneNumber(regSession.getPhoneNumber());
+        if (profileEntity == null) {
+            long time = System.currentTimeMillis();
+            profileEntity = profileService.save(new ProfileEntity(regSession.getPhoneNumber(),
+                    time, time, null, null, null));
+            profileSettingsService.save(new ProfileSettingsEntity(profileEntity.getId(), true, true));
+            profileStatusService.save(new ProfileStatusEntity(profileEntity.getId(), null));
         }
+        String deviceToken = tokenHandler.createDeviceToken(profileEntity.getId(), regSession.getDeviceId());
+        DeviceEntity deviceEntity = deviceService.findByProfileIdAndDeviceId(profileEntity.getId(), regSession.getDeviceId());
+        if (deviceEntity == null)
+            deviceEntity = new DeviceEntity(profileEntity.getId(), regSession.getDeviceId(), null);
+        deviceEntity.setConfirmationTime(tokenHandler.getClaimsFromDeviceToken(deviceToken).getIssuedAt().getValueInMillis());
+        deviceService.save(deviceEntity);
 
-        return new Response<>(header, data, status);
+        return ResponseFactory.createResponse(msg, new SmsConfirmResponse(deviceToken));
     }
 }
