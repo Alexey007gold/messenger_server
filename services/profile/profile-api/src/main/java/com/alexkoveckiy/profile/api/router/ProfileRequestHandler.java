cd957fb387157a86b7d87dec061fb5f4b57fc598
Alex Kovetskiy <Alexey007gold@mail.ru>
2017-04-07 16:10:26 +0300

    ContactSync realization

--- services/profile/profile-api/src/main/java/com/alexkoveckiy/profile/api/router/ProfileRequestHandler.java
+++ services/profile/profile-api/src/main/java/com/alexkoveckiy/profile/api/router/ProfileRequestHandler.java
@@ -1,6 +1,5 @@
 package com.alexkoveckiy.profile.api.router;
 
-import com.alexkoveckiy.common.protocol.ResponseData;
 import com.alexkoveckiy.common.router.api.RequestHandler;
 
 public interface ProfileRequestHandler extends RequestHandler {
--- services/profile/profile-impl/src/main/java/com/alexkoveckiy/profile/impl/handler/ContactSyncRequestHandler.java
+++ services/profile/profile-impl/src/main/java/com/alexkoveckiy/profile/impl/handler/ContactSyncRequestHandler.java
@@ -1,12 +1,20 @@
 package com.alexkoveckiy.profile.impl.handler;
 
+import com.alexkoveckiy.common.dao.entities.ContactEntity;
+import com.alexkoveckiy.common.dao.entities.NumberEntity;
+import com.alexkoveckiy.common.dao.service.ContactService;
+import com.alexkoveckiy.common.dto.configuration.DTO.ContactDTO;
 import com.alexkoveckiy.common.protocol.Request;
 import com.alexkoveckiy.common.protocol.Response;
+import com.alexkoveckiy.common.protocol.ResponseFactory;
 import com.alexkoveckiy.common.router.api.AbstractRequestHandler;
 import com.alexkoveckiy.profile.api.message.ContactSyncRequest;
 import com.alexkoveckiy.profile.api.message.ContactSyncResponse;
 import com.alexkoveckiy.profile.api.router.ProfileRequestHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.*;
 
 /**
  * Created by alex on 24.02.17.
@@ -16,6 +24,8 @@ import org.springframework.stereotype.Component;
 public class ContactSyncRequestHandler extends AbstractRequestHandler<ContactSyncRequest, ContactSyncResponse>
         implements ProfileRequestHandler {
 
+    @Autowired
+    private ContactService contactService;
 
     @Override
     public String getName() {
@@ -23,7 +33,67 @@ public class ContactSyncRequestHandler extends AbstractRequestHandler<ContactSyn
     }
 
     @Override
-    protected Response<ContactSyncResponse> process(Request<ContactSyncRequest> msg) {
-        return null;
+    public Response<ContactSyncResponse> process(Request<ContactSyncRequest> msg) {
+        addContacts(msg.getRoutingData().getProfileId(), msg.getData().getAddedContacts());
+        removeContacts(msg.getRoutingData().getProfileId(), msg.getData().getRemovedContacts());
+
+        return ResponseFactory.createResponse(msg, new ContactSyncResponse());
+    }
+
+    private void addContacts(String userId, List<ContactDTO> contacts) {
+        if (contacts == null) return;
+
+        Set<NumberEntity> existingNumberEntities;
+        String contactId;
+        ContactEntity contactEntity;
+
+        for (ContactDTO contactDTO : contacts) {
+            contactId = UUID.randomUUID().toString();
+            contactEntity = contactService.findByProfileIdAndName(userId, contactDTO.getName());
+
+            if (contactEntity != null) {
+                existingNumberEntities = contactEntity.getNumbers();
+                //remove existing numbers from new numbers set
+                for (NumberEntity existingEntity : existingNumberEntities)
+                    contactDTO.getNumbers().remove(existingEntity.getNumber());
+                //create and add new numberEntities to existing
+                for (String newNumber : contactDTO.getNumbers())
+                    existingNumberEntities.add(new NumberEntity(contactId, newNumber));
+                //save
+                contactService.save(contactEntity);
+            } else {
+                Set<NumberEntity> newNumberEntities = new HashSet<>();
+                for (String newNumber : contactDTO.getNumbers())
+                    newNumberEntities.add(new NumberEntity(contactId, newNumber));
+
+                contactService.save(new ContactEntity(userId, contactDTO.getName(), newNumberEntities));
+            }
+        }
+    }
+
+    private void removeContacts(String userId, List<ContactDTO> contacts) {
+        if (contacts == null) return;
+
+        NumberEntity entity;
+        ContactEntity contactEntity;
+        Iterator<NumberEntity> existingNumberEntitiesIterator;
+
+        for (ContactDTO contactDTO : contacts) {
+            //find contactEntity
+            contactEntity = contactService.findByProfileIdAndName(userId, contactDTO.getName());
+            if (contactEntity != null) {
+                existingNumberEntitiesIterator = contactEntity.getNumbers().iterator();
+                //remove needed
+                while (existingNumberEntitiesIterator.hasNext()) {
+                    entity = existingNumberEntitiesIterator.next();
+                    if (contactDTO.getNumbers().contains(entity.getNumber()))
+                        existingNumberEntitiesIterator.remove();
+                }
+                if (contactEntity.getNumbers().isEmpty())
+                    contactService.delete(contactEntity.getId());
+                else
+                    contactService.save(contactEntity);
+            }
+        }
     }
 }
