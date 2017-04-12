8212cc364b5bf1b3d7a02083e233967e88a858ef
Alex Kovetskiy <Alexey007gold@mail.ru>
2017-04-12 16:39:31 +0300

    Renamed Status enum to ResponseStatusEnum and added message parameter to it

--- basis/common-protocol/src/main/java/com/alexkoveckiy/common/protocol/ResponseFactory.java
+++ basis/common-protocol/src/main/java/com/alexkoveckiy/common/protocol/ResponseFactory.java
@@ -2,7 +2,7 @@ package com.alexkoveckiy.common.protocol;
 
 import java.util.UUID;
 
-import static com.alexkoveckiy.common.protocol.ResponseFactory.Status.OK;
+import static com.alexkoveckiy.common.protocol.ResponseFactory.ResponseStatusEnum.OK;
 
 /**
  * Created by alex on 06.04.17.
@@ -13,14 +13,14 @@ public class ResponseFactory {
         Response<T> response = new Response<>();
         setHeader(req, response);
         response.setData(data);
-        response.setStatus(new ResponseStatus(OK.code, OK.name()));
+        response.setStatus(new ResponseStatus(OK.code, OK.message));
         return response;
     }
 
-    public static <T extends ResponseData> Response<T> createResponse(Request<?> req, Status status) {
+    public static <T extends ResponseData> Response<T> createResponse(Request<?> req, ResponseStatusEnum status) {
         Response<T> response = new Response<>();
         setHeader(req, response);
-        response.setStatus(new ResponseStatus(status.code, status.name()));
+        response.setStatus(new ResponseStatus(status.code, status.message));
         return response;
     }
 
@@ -28,14 +28,14 @@ public class ResponseFactory {
         Response<T> response = new Response<>();
         response.setHeader(header);
         response.setData(data);
-        response.setStatus(new ResponseStatus(OK.code, OK.name()));
+        response.setStatus(new ResponseStatus(OK.code, OK.message));
         return response;
     }
 
-    public static <T extends ResponseData> Response<T> createResponse(Request<?> req, Status status, String message) {
+    public static <T extends ResponseData> Response<T> createResponse(Request<?> req, ResponseStatusEnum status, String message) {
         Response<T> response = new Response<>();
         setHeader(req, response);
-        response.setStatus(new ResponseStatus(status.code, status.name() + " (" + message + ")"));
+        response.setStatus(new ResponseStatus(status.code, status.message + " (" + message + ")"));
         return response;
     }
 
@@ -44,25 +44,31 @@ public class ResponseFactory {
                 req.getHeader().getCommand(), req.getHeader().getType(), req.getHeader().getVersion()));
     }
 
-    public enum Status {
-        OK(200),
-        BAD_REQUEST(400),
-        FORBIDDEN(403),
-        NOT_FOUND(404),
-        NOT_ACCEPTABLE(422),
-        UNPROCESSABLE_ENTITY(422),
-        TOO_MANY_REQUESTS(429),
-        INTERNAL_SERVER_ERROR(500),
-        NOT_IMPLEMENTED(501);
+    public enum ResponseStatusEnum {
+        OK(200, "Ok"),
+        BAD_REQUEST(400, "Bad request"),
+        FORBIDDEN(403, "Forbidden"),
+        NOT_FOUND(404, "Not found"),
+        NOT_ACCEPTABLE(422, "Not acceptable"),
+        UNPROCESSABLE_ENTITY(422, "Unprocessable entity"),
+        TOO_MANY_REQUESTS(429, "Too many requests"),
+        INTERNAL_SERVER_ERROR(500, "Internal server error"),
+        NOT_IMPLEMENTED(501, "Not implemented");
 
         private final int code;
+        private final String message;
 
-        Status(int code) {
+        ResponseStatusEnum(int code, String message) {
             this.code = code;
+            this.message = message;
         }
 
         public int getCode() {
             return code;
+        }
+
+        public String getMessage() {
+            return message;
         }
     }
 }
