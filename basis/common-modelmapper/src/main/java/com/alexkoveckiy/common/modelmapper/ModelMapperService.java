3a110ff1893edee0a5e21c44584ddf146d383b66
Alex Kovetskiy <Alexey007gold@mail.ru>
2017-04-13 10:58:36 +0300

    set matchingStrategy to STRICT

--- basis/common-modelmapper/src/main/java/com/alexkoveckiy/common/modelmapper/ModelMapperService.java
+++ basis/common-modelmapper/src/main/java/com/alexkoveckiy/common/modelmapper/ModelMapperService.java
@@ -1,9 +1,7 @@
 package com.alexkoveckiy.common.modelmapper;
 
-import com.alexkoveckiy.common.dao.entities.ProfileStatusEntity;
-import com.alexkoveckiy.common.dto.configuration.DTO.ProfileStatusDTO;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,16 +14,7 @@ public class ModelMapperService {
 
     public ModelMapperService() {
         modelMapper = new ModelMapper();
-        //this mapping is needed because without it mapper
-        //tries to convert wrong field to boolean
-        //and throws exception that Long can't be
-        //converted to boolean
-        modelMapper.addMappings(new PropertyMap<ProfileStatusEntity, ProfileStatusDTO>() {
-            @Override
-            protected void configure() {
-                map().setOnline(false);
-            }
-        });
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
     }
 
     public <D> D map(Object source, Class<D> destinationType) {
